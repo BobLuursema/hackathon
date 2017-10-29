057b5919d88727ca5ae8269e423b14959cb4f243
Bob Luursema <bob.luursema@outlook.com>
2017-10-29 08:34:21 +0100

    new version

--- calco1/simple/urls.py
+++ calco1/simple/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^regelingen/$', views.regelingen),
     url(r'^suggesties/(?P<id>[0-9]+)/$', views.suggesties),
+    url(r'^persoon/(?P<id>[0-9]+)/$', views.persoon),
 ]
--- calco1/simple/views.py
+++ calco1/simple/views.py
@@ -5,38 +5,30 @@ import operator
 
 # Create your views here.
 def regelingen(request):
-    data = m.RegelingOpties.objects.values()
-    return JsonResponse(dict(regelingopties=list(data)))
+    data = m.Regelingen.objects.values_list('naam', flat=True)
+    return JsonResponse(dict(regelingen=list(data)))
+
+def persoon(request, id):
+    p_naam = m.Persoon.objects.get(pk=id).naam
+    p_regelingen = m.Persoon.objects.get(pk=id).regelingen.values_list('naam', flat=True)
+    return JsonResponse(dict(naam=p_naam, regelingen=list(p_regelingen)))
 
 def suggesties(request, id):
     p = m.Persoon.objects.get(pk=id)
-    reg = {k: {'aanvraag': 15, 'wijzigen': 0, 'informatie': 0} for k in list(m.Regelingen.objects.values_list('naam', flat=True))}
+    reg = {k: 0 for k in list(m.Regelingen.objects.values_list('naam', flat=True))}
     for r in p.regelingen.all():
-        reg[r.naam]['aanvraag'] -= 500
-        reg[r.naam]['wijzigen'] += 100
+        reg[r.naam] -= 500
     if p.ontslagaanvraag:
-        reg['WW']['aanvraag'] += 10
-        reg['WW']['informatie'] += 20
+        reg['WW'] += 20
     if p.leeftijd > 40:
-        reg['Ziekte']['aanvraag'] += 10
-        reg['Ziekte']['informatie'] += 20
-        reg['Zwanger']['aanvraag'] -= 15
-        reg['Zwanger']['informatie'] -= 15
+        reg['Ziekte'] += 20
+        reg['Zwanger'] -= 15
     if p.geslacht == 'V':
-        reg['Zwanger']['aanvraag'] += 10
-        reg['Zwanger']['informatie'] += 20
-    semi_results = {}
-    for r in reg:
-        for f in reg[r]:
-            semi_results["{} {}".format(r,f)] = reg[r][f]
+        reg['Zwanger'] += 20
     results = {'data':[]}
-    for k, v in sorted(semi_results.items(), key=operator.itemgetter(1)):
-        if k[k.index(' ')+1:] == 'aanvraag':
-            url = m.Regelingen.objects.get(naam=k[:k.index(' ')]).insert_url
-        elif k[k.index(' ')+1:] == 'informatie':
-            url = m.Regelingen.objects.get(naam=k[:k.index(' ')]).info_url
-        elif k[k.index(' ')+1:] == 'wijzigen':
-            url = m.Regelingen.objects.get(naam=k[:k.index(' ')]).update_url
+    for k, v in sorted(reg.items(), key=operator.itemgetter(1)):
+        url = m.Regelingen.objects.get(naam=k).info_url
         results['data'].append({k: url})
     results['data'].reverse()
+    results['data'] = results['data'][:2]
     return JsonResponse(results)
